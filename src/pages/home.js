01733bc66ec997752a517cb7fab9f514a5b07e33
Cristian Andres Fernandez Bermudez <crafernandezbe@unal.edu.co>
2021-06-20 21:34:49 -0500

    photo

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import photo from "../selfie1.webp";
 
 function Home() {
   React.useEffect(() => {
@@ -7,13 +8,23 @@ function Home() {
   return (
     <main>
       <section className="section">
-        <h1 className="title">Hi, I'm Cristian Andres Fernandez</h1>
-        <p>
-          I'm a geologist from Colombia and I'm just beginning my
-          career in web development. I'm currently learning
-          programming from the MinTic so I can get solid fundamentals
-          on programming and translate it to web development.
-        </p>
+        <h1 className="title">Hi, I'm Cristian Fernandez</h1>
+        <div className="description">
+          <div className="image-container">
+            <img
+              src={photo}
+              alt="Cristian Fernandez"
+              className="photo"
+            />
+          </div>
+          <p>
+            I'm a geologist from Colombia and I'm just beginning my
+            career in web development. I'm currently learning
+            programming from the MinTic so I can get solid
+            fundamentals on programming and translate them to web
+            development.
+          </p>
+        </div>
       </section>
     </main>
   );
