ab171d6fb19fef6205100a8076ead3ee054f589a
Cristian Andres Fernandez Bermudez <crafernandezbe@unal.edu.co>
2021-06-22 21:41:28 -0500

    lazy and suspense in router

--- src/App.js
+++ src/App.js
@@ -1,49 +1,54 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import {
   BrowserRouter as Router,
   Route,
   Switch,
 } from "react-router-dom";
-import Home from "./pages/home";
-import About from "./pages/about";
-import Contact from "./pages/contact";
-import Error from "./pages/error";
-import Navbar from "./components/navbar";
-import Projects from "./pages/Projects";
-import Pokemon from "./pages/Pokemon";
-import PokemonChild from "./components/PokemonChild";
 import AccessibleNavigationAnnouncer from "./components/AccesibleNavigationAnnouncer";
+import Loader from "./components/loader";
+import Navbar from "./components/navbar";
+
+const Home = lazy(() => import("./pages/home"));
+const About = lazy(() => import("./pages/about"));
+const Contact = lazy(() => import("./pages/contact"));
+const Error = lazy(() => import("./pages/error"));
+const Projects = lazy(() => import("./pages/Projects"));
+const Pokemon = lazy(() => import("./pages/Pokemon"));
+const PokemonChild = lazy(() => import("./components/PokemonChild"));
+
 function App() {
   return (
-    <div>
+    <>
       <Router>
         <AccessibleNavigationAnnouncer />
         <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route exact path="/projects">
-            <Projects />
-          </Route>
-          <Route exact path="/projects/pokemon">
-            <Pokemon />
-          </Route>
-          <Route path="/projects/pokemon/:name">
-            <PokemonChild />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="*">
-            <Error />
-          </Route>
-        </Switch>
+        <Suspense fallback={<Loader />}>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route exact path="/projects">
+              <Projects />
+            </Route>
+            <Route exact path="/projects/pokemon">
+              <Pokemon />
+            </Route>
+            <Route path="/projects/pokemon/:name">
+              <PokemonChild />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="*">
+              <Error />
+            </Route>
+          </Switch>
+        </Suspense>
       </Router>
-    </div>
+    </>
   );
 }
 
