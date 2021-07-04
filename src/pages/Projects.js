d9fc2f36b492ccf66a10415f4fdd700a4a54c2b5
Cristian Andres Fernandez Bermudez <crafernandezbe@unal.edu.co>
2021-07-04 14:55:01 -0500

    project style

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -12,38 +12,60 @@ function Projects() {
       <section className="section">
         <h2 className="title">Projects</h2>
         <p>
-          I've been following a lot of tutorials right now, so I will hopefully
-          add more projects in the near future.
+          I've been studying a lot of courses and tutorials about React,
+          javascript, and some libraries with which I will hopefully add more
+          projects in the near future.
         </p>
-        <p>Check out my pokemon database: </p>
-        <Link
-          to="/projects/pokemon"
-          className="poke-link"
-          style={{ marginBottom: "0.7rem" }}
-        >
-          Pokemon Database
-        </Link>
-        <p>
-          Check out my Instagram Clone done with React, Tailwind CSS and
-          Firebase
-        </p>
-        <a
-          href="https://instagram-clone-arch.vercel.app/"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          <picture>
-            <source type="image/webp" srcSet={Instagramw} />
-            <img
-              type="image/jgp"
-              src={Instagramj}
-              alt="Instagram clone login page"
-              style={{ border: "1px solid gray", borderRadius: "0.5rem" }}
-              width="276"
-              height="223"
-            />
-          </picture>
-        </a>
+        <div className="projects">
+          <div
+            style={{
+              display: "flex",
+              flexFlow: "column wrap",
+              alignItems: "center",
+            }}
+          >
+            <p>
+              Check out my pokemon database done with the Pokeapi, and basic
+              React hooks.
+            </p>
+            <Link
+              to="/projects/pokemon"
+              className="poke-link"
+              style={{ marginBottom: "0.7rem" }}
+            >
+              Pokemon Database
+            </Link>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexFlow: "column wrap",
+              alignItems: "center",
+            }}
+          >
+            <p>
+              Check out my Instagram Clone done with React, Tailwind CSS and
+              Firebase.
+            </p>
+            <a
+              href="https://instagram-clone-arch.vercel.app/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <picture>
+                <source type="image/webp" srcSet={Instagramw} />
+                <img
+                  type="image/jgp"
+                  src={Instagramj}
+                  alt="Instagram clone login page"
+                  style={{ border: "1px solid gray", borderRadius: "0.5rem" }}
+                  width="276"
+                  height="223"
+                />
+              </picture>
+            </a>
+          </div>
+        </div>
       </section>
     </main>
   );
