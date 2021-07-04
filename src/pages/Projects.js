79f6a7a0a5f8adbae1c38356f787df4dfc5ef193
Cristian Andres Fernandez Bermudez <crafernandezbe@unal.edu.co>
2021-07-04 14:18:28 -0500

    instagram project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Instagramw from "../instagramlogin.webp";
+import Instagramj from "../instagramlogin.jpg";
 
 function Projects() {
   React.useEffect(() => {
@@ -10,13 +12,38 @@ function Projects() {
       <section className="section">
         <h2 className="title">Projects</h2>
         <p>
-          I've been following a lot of tutorials right now, so I will
-          hopefully add more projects in the near future.
+          I've been following a lot of tutorials right now, so I will hopefully
+          add more projects in the near future.
         </p>
         <p>Check out my pokemon database: </p>
-        <Link to="/projects/pokemon" className="poke-link">
+        <Link
+          to="/projects/pokemon"
+          className="poke-link"
+          style={{ marginBottom: "0.7rem" }}
+        >
           Pokemon Database
         </Link>
+        <p>
+          Check out my Instagram Clone done with React, Tailwind CSS and
+          Firebase
+        </p>
+        <a
+          href="https://instagram-clone-arch.vercel.app/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <picture>
+            <source type="image/webp" srcSet={Instagramw} />
+            <img
+              type="image/jgp"
+              src={Instagramj}
+              alt="Instagram clone login page"
+              style={{ border: "1px solid gray", borderRadius: "0.5rem" }}
+              width="276"
+              height="223"
+            />
+          </picture>
+        </a>
       </section>
     </main>
   );
